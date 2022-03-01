63943f730bc1fc70aabb5506086a86eb3719fca2
hoovivaf2e <hooviva77@gmail.com>
2022-03-01 18:07:30 +0800

    update week 4

--- week4/js/pagination.js
+++ week4/js/pagination.js
@@ -4,15 +4,15 @@ export default {
     <nav aria-label="Page navigation example">
         <ul class="pagination d-md-flex justify-content-center">
             <li class="page-item" :class="{ 'disabled': !pages.has_pre }">
-                <a class="page-link" href="#" @click="$emit('get-product', pages.current_page - 1)" aria-label="Previous">
+                <a class="page-link" href="#" @click.prevent="$emit('get-product', pages.current_page - 1)" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                 </a>
             </li>
             <li class="page-item" :class="{ active: page === pages.current_page }" v-for="page in pages.total_pages" :key="page+'page'">
-                <a class="page-link" href="#" @click="$emit('get-product', page)">{{ page }}</a>
+                <a class="page-link" href="#" @click.prevent="$emit('get-product', page)">{{ page }}</a>
             </li>
             <li class="page-item" :class="{ 'disabled': !pages.has_next }">
-                <a class="page-link" href="#" @click="$emit('get-product', pages.current_page + 1)" aria-label="Next">
+                <a class="page-link" href="#" @click.prevent="$emit('get-product', pages.current_page + 1)" aria-label="Next">
                     <span aria-hidden="true">&raquo;</span>
                 </a>
             </li>

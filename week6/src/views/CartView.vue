09f196d3280803ca7cc088b3338ea157141a890f
hoovivaf2e <hooviva77@gmail.com>
2022-03-03 17:18:41 +0800

    update settings

--- week6/src/views/CartView.vue
+++ week6/src/views/CartView.vue
@@ -76,7 +76,10 @@
     <div class="my-5 row justify-content-center">
       <Form ref="form" class="col-md-6" v-slot="{ errors }" @submit="createOrder">
         <div class="mb-3">
-          <label for="email" class="form-label">Email</label>
+          <span class="text-danger">*</span> 為必填
+        </div>
+        <div class="mb-3">
+          <label for="email" class="form-label"><span class="text-danger">*</span>Email</label>
           <Field id="email" name="email" type="email" class="form-control"
             :class="{ 'is-invalid': errors['email'] }"
             v-model="form.user.email"
@@ -87,7 +90,7 @@
         </div>
 
         <div class="mb-3">
-          <label for="name" class="form-label">收件人姓名</label>
+          <label for="name" class="form-label"><span class="text-danger">*</span>收件人姓名</label>
           <Field id="name" name="姓名" type="text" class="form-control"
             :class="{ 'is-invalid': errors['姓名'] }"
             v-model="form.user.name"
@@ -98,18 +101,18 @@
         </div>
 
         <div class="mb-3">
-          <label for="tel" class="form-label">收件人電話</label>
-          <Field id="tel" name="電話" type="text" class="form-control"
+          <label for="tel" class="form-label"><span class="text-danger">*</span>收件人電話</label>
+          <Field id="tel" name="電話" type="tel" class="form-control"
             v-model="form.user.tel"
             :class="{'is-invalid': errors['電話']}"
             placeholder="請輸入電話"
-            rules="required"
+            rules="required|min:8"
           ></Field>
           <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
         </div>
 
         <div class="mb-3">
-          <label for="address" class="form-label">收件人地址</label>
+          <label for="address" class="form-label"><span class="text-danger">*</span>收件人地址</label>
           <Field id="address" name="地址" type="text" class="form-control"
             :class="{'is-invalid': errors['地址']}"
             v-model="form.user.address"
